5b5fb3a71d5ace7b03bf8d94a3d90d9629c541ae
vladus80 <utru@inbox.ru>
2020-11-10 16:55:47 +0300

    Интерфейс IWorkSource

---
+++ src/com/company/IWorkSource.java
@@ -0,0 +1,13 @@
+
+/*
+Интерфейс определяет метод  getStringRaw
+ */
+
+package com.company;
+import java.io.IOException;
+
+public interface IWorkSource {
+
+    String getStringRaw() throws IOException;
+
+}
