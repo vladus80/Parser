5e18897320c1bd58cad08af4f4a53421c00cdcd5
vladus80 <utru@inbox.ru>
2020-11-10 16:57:22 +0300

    Обновил в соответстии новой концепцией кода - работа через интерфес

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,18 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Parser parser = new Parser("https://mail.ru", "");
-        String list = parser.getStringRaw();
-        System.out.println(parser.getLinks(list).toString());
 
-        for (String link: parser.getLinks(list)) {
-            System.out.println(link);
-        }
+        String pattern = "(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+
+        IWorkSource SourceFile = new SourceFile("C:\\Users\\vladus\\ursa.html");
+        new Parser(SourceFile, "C:\\Users\\vladus\\dataFile.txt", pattern).saveDataInFile();
+
+
+        IWorkSource sourceNet = new SourceNet("https://mail.ru");
+        new Parser(sourceNet, "C:\\Users\\vladus\\dataInet.txt", pattern).saveDataInFile();
+
 
     }
 }
